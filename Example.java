83b632bf6e27578b1410cd0819e0e7f1f5f9d86d
calebcordek <calebcordek@gmail.com>
2026-04-06 11:12:11 -0400

    File

---
+++ Example.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Example {
+    public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in);
+        int i = scnr.nextInt();
+        
+        System.out.println("Your num is " + i);
+    }
+}
